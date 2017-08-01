1ca78d370b917c86cbf9604f50d4e811b9ca6975
samuelagyemang2012 <khermz2012@gmail.com>
2017-08-01 19:03:02 +0100

    changes for today

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="/eswift">
                         ESwift
                     </a>
                 </div>
