9b7be5b80148b6da9b62b782ce31838ed77af88b
samuelagyemang2012 <khermz2012@gmail.com>
2017-08-24 18:24:21 +0100

    jk

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,15 +15,9 @@ class ApiController extends Controller
 
         $data = $p->get_maximum($name);
 
-//        $registration_fee = $fee_percentage * $data[0]->maximum;
+        $registration_fee = $fee_percentage * $data[0]->maximum;
 
-        $registration_fee = "sdadsa";
-
-//        $minimum = $registration_fee;// + $mobile_fee;
-
-//        return $minimum;
-
-        return response()->json(['code' => 1, 'minimum' => $data[0]->maximum]);
+        return response()->json(['code' => 1, 'minimum' => $registration_fee]);
     }
 
 }
