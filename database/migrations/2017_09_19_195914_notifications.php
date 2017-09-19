b6296ac0fd8ff0b5d63cf537f7773dc99dd94270
samuelagyemang2012 <khermz2012@gmail.com>
2017-09-19 21:08:50 +0100

    notify

---
+++ database/migrations/2017_09_19_195914_notifications.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Notifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('telephone');
+            $table->string('message');
+            $table->integer('is_read');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
