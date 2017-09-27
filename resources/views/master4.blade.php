9723d444dcf500fab8adca9c05bf4efd73c3df45
samuelagyemang2012 <khermz2012@gmail.com>
2017-09-27 11:43:29 +0100

    jk

--- resources/views/master4.blade.php
+++ resources/views/master4.blade.php
@@ -338,21 +338,21 @@
                 </div>
             </div>
 
-            <form action="#" method="get" class="sidebar-form">
-                <div class="input-group">
-                    <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-                </div>
-            </form>
+            {{--<form action="#" method="get" class="sidebar-form">--}}
+            {{--<div class="input-group">--}}
+            {{--<input type="text" name="q" class="form-control" placeholder="Search...">--}}
+            {{--<span class="input-group-btn">--}}
+            {{--<button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>--}}
+            {{--</button>--}}
+            {{--</span>--}}
+            {{--</div>--}}
+            {{--</form>--}}
 
             <ul class="sidebar-menu">
 
                 <li class="header"></li>
-                <li><a href="{{route('transactions_pending_loans')}}"><i class="fa fa-television"></i>
-                        <span>Dashboard</span></a></li>
+{{--                <li><a href="{{route('transactions_pending_loans')}}"><i class="fa fa-television"></i>--}}
+                        {{--<span>Dashboard</span></a></li>--}}
 
                 <li class="active treeview">
                     <a href="{{route('super_admin_home')}}">
