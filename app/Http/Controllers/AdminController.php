ca92dd5dc054a3bcb9fd39df9dfbd0fc3d55e503
samuelagyemang2012 <khermz2012@gmail.com>
2017-10-05 13:46:31 +0100

    jk

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -796,6 +796,7 @@ class AdminController extends Controller
 
 
         if ($post_e_balance[0]->balance > $cur_m_balance[0]->balance) {
+
             $message = "Your Eswift account has been credited with GHC " . round($dec_e, 2);
             $s->send($msisdn, $message);
         } else {
