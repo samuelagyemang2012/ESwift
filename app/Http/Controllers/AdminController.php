b3be078f72499966acfaf4efdb5e8e2900c36670
samuelagyemang2012 <khermz2012@gmail.com>
2017-10-05 13:58:54 +0100

    jk

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -795,12 +795,12 @@ class AdminController extends Controller
         $dec_m = $post_m_balance[0]->balance - $cur_m_balance[0]->balance;
 
 
-        if ($post_e_balance[0]->balance > $cur_m_balance[0]->balance) {
+        if ($post_e_balance[0]->balance > $cur_e_balance[0]->balance) {
 
             $message = "Your Eswift account has been credited with GHC " . round($dec_e, 2);
             $s->send($msisdn, $message);
 
-        } elseif ($post_e_balance[0]->balance < $cur_m_balance[0]->balance) {
+        } elseif ($post_e_balance[0]->balance < $cur_e_balance[0]->balance) {
             $message = "Your Eswift account has been debited with GHC " . round($dec_e, 2);
             $s->send($msisdn, $message);
         }
