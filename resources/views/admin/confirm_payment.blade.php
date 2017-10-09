2b40ad789802b4d9c61922301ed497af9478388b
samuelagyemang2012 <khermz2012@gmail.com>
2017-10-09 17:00:51 +0100

    jk

--- resources/views/admin/confirm_payment.blade.php
+++ resources/views/admin/confirm_payment.blade.php
@@ -83,7 +83,7 @@
                     <label>Amount Paid</label>
                     <div>
                         <input class="form-control" name="amount_paid" type="number" required
-                               value="" value="{{old('amount_paid')}}" min="2">
+                               value="" value="{{old('amount_paid')}}" min="0.5">
                     </div>
                     <br>
 
